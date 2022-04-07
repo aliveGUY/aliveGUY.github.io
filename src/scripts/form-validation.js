a82784780c79d12e34dc3b4d0987d7015fae625e
aliveGUY <ilyadubrivvin@gmail.com>
2022-04-08 00:44:35 +0300

    Code polishing

--- src/scripts/form-validation.js
+++ src/scripts/form-validation.js
@@ -1,8 +1,14 @@
-const data = document.querySelectorAll('#Form-form input, textarea');
+let data = document.querySelectorAll('#Form-form input, textarea');
 const error = document.querySelectorAll('#Form-form small');
 const form = document.querySelector('#Form-form');
 
-console.log(data)
+let ei = 2;
+
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+  data[1].value = 'sent from mobile';
+  data = document.querySelectorAll('#Name, #email, #message');
+  ei = 1;
+}
 
 form.addEventListener('submit', (e) => {
   for (let i = 0; i < data.length; i += 1) {
@@ -20,16 +26,16 @@ form.addEventListener('submit', (e) => {
     }
   }
 
-  if (data[2].value.toLowerCase() !== data[2].value) {
+  if (data[ei].value.toLowerCase() !== data[ei].value) {
     e.preventDefault();
-    data[2].classList.add('error')
-    error[2].textContent = 'Email cant contain upercase';
+    data[ei].classList.add('error');
+    error[ei].textContent = 'Email cant contain upercase';
     error[4].textContent = 'Email cant contain upercase';
     e.preventDefault();
     e.preventDefault();
     e.preventDefault();
   } else {
-    error[2].textContent = '';
-    data[2].classList.remove('error');
+    error[ei].textContent = '';
+    data[ei].classList.remove('error');
   }
 });
